567933e631e7f788d319e7caeefd789925e46c5f
YOON <qwer1234ws30@gmail.com>
2024-10-10 17:54:46 +0900

    'studying_commit'

---
+++ git_setumei.py
@@ -0,0 +1,9 @@
+# プル(updateさせる）、プッシュ(updateする)
+
+#stageの概念(saveの感じ)
+    #. 
+    # git add. (現在の（全部）ファイルをstatusされる（commitの対象を追加する感じ))
+# clone: git clone UIR
+
+def main ():
+    print("hello git_hub!!")
