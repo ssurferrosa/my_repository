5a6012a48e43cf78fc0056f03ee2846af4f8b4e3
YOON <qwer1234ws30@gmail.com>
2024-10-10 17:58:25 +0900

    'study3'

--- git_setumei.py
+++ git_setumei.py
@@ -1,6 +1,6 @@
 # プル(updateさせる）、プッシュ(updateする)
 
-# # clone: git clone UIR
+# # clone: git clone UIR (紐づけの観点でこれが便利)
 
 #statusの概念(saveの感じ)
     #. 3
@@ -8,6 +8,7 @@
     # git status で確認できる
 #commit (add した後で出来る)
     # git commit - m '話したいメッセージ'
+    # git push 
 
 
 
